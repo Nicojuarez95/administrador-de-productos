c7a79da6a1b854b695bbb55388983dd730079dbb
Nico <juareznico61@gmail.com>
2023-05-02 22:07:01 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { Product } from "./Product.js";
 import { UI } from "./UI.js";
 
-// DOM Events
+function callAddProduct(){
+  let products = JSON.parse(localStorage.getItem("products")) || [];
+  const ui = new UI();
+  for(let product of products){
+    ui.addHtml(product)
+  }
+}
+callAddProduct()
+
 document
   .getElementById("product-form")
   .addEventListener("submit", function (e) {
@@ -13,25 +21,32 @@ document
       price = document.getElementById("price").value,
       year = document.getElementById("year").value;
 
+    // Input User Validation
+    if (name === "" || price === "" || year === "") {
+      const ui = new UI();
+      ui.showMessage("Por favor inserte todos los datos", "danger");
+      return; // No crea un nuevo producto si hay campos vacíos
+    }
+
     // Create a new Oject Product
     const product = new Product(name, price, year);
 
     // Create a new UI instance
     const ui = new UI();
 
-    // Input User Validation
-    if (name === "" || price === "" || year === "") {
-      ui.showMessage("Please Insert data in all fields", "danger");
-    }
-
     // Save Product
     ui.addProduct(product);
-    ui.showMessage("Product Added Successfully", "success");
+    ui.showMessage("Producto agregado correctamente", "success");
     ui.resetForm();
   });
 
-document.getElementById("product-list").addEventListener("click", (e) => {
-  const ui = new UI();
-  ui.deleteProduct(e.target);
-  e.preventDefault();
-});
+// Evento click del botón de eliminación
+document
+  .getElementById("product-list")
+  .addEventListener("click", (event) => {
+    const ui = new UI();
+    ui.deleteProduct(event.target);
+  });
+
+
+
--- src/UI.js
+++ src/UI.js
@@ -1,39 +1,170 @@
-/**
- * User Interface Class
- */
 export class UI {
-  /**
-   * Add a New Product 
-   * @param {Object} product A new product Object
-   */
-  addProduct(product) {
+  addHtml(product){
+    const { name, price, year } = product;
     const productList = document.getElementById("product-list");
+    const id = Date.now().toString();
     const element = document.createElement("div");
+    element.setAttribute("id", id);
     element.innerHTML = `
-            <div class="card text-center mb-4">
-                <div class="card-body">
-                    <strong>Product</strong>: ${product.name} -
-                    <strong>Price</strong>: ${product.price} - 
-                    <strong>Year</strong>: ${product.year}
-                    <a href="#" class="btn btn-danger" name="delete">Delete</a>
-                </div>
-            </div>
-        `;
+      <div class="card text-center mb-4">
+        <div class="producto">
+          <div class="card-objet">
+            <strong>Producto</strong>: ${name} -
+            <strong>Precio</strong>: <span class="product-price">${price}</span> - 
+            <strong>Año</strong>: ${year}
+          </div>
+          <div class="contbtn">
+            <button class="btn btn-danger delete-product" id=${id} name="delete">Eliminar producto</button>
+            <button class="btn btn-primary edit-product" name="edit">Editar precio</button>
+            <button class="btn btn-success save-product" name="save" style="display: none;">Guardar</button>
+          </div>
+        </div>
+        <div class="card-footer">
+          <input type="number" class="form-control edit-price" style="display: none;">
+        </div>
+      </div>
+    `;
     productList.appendChild(element);
   }
+  addProduct(product) {
+    const { name, price, year } = product;
+    const productList = document.getElementById("product-list");
+    const id = Date.now().toString();
+    const element = document.createElement("div");
+    element.setAttribute("id", id);
+    element.innerHTML = `
+      <div class="card text-center mb-4">
+        <div class="producto">
+          <div class="card-objet">
+            <strong>Producto</strong>: ${name} -
+            <strong>Precio</strong>: <span class="product-price">${price}</span> - 
+            <strong>Año</strong>: ${year}
+          </div>
+          <div class="contbtn">
+            <button class="btn btn-danger delete-product" id=${id} name="delete">Eliminar producto</button>
+            <button class="btn btn-primary edit-product" name="edit">Editar precio</button>
+            <button class="btn btn-success save-product" name="save" style="display: none;">Guardar</button>
+          </div>
+        </div>
+        <div class="card-footer">
+          <input type="number" class="form-control edit-price" style="display: none;">
+        </div>
+      </div>
+    `;
+    productList.appendChild(element);
 
-  /**
+    // Crea un objeto para el producto y lo agrega al Local Storage
+    const productObj = { id, name, price, year };
+    let products = JSON.parse(localStorage.getItem("products")) || [];
+    products.push(productObj);
+    localStorage.setItem("products", JSON.stringify(products));
+
+    this.showMessage("Producto agregado correctamente", "success");
+    this.resetForm();
+  
+    //EVENTO AL BOTON EDITAR PRODUCTO//
+
+    // Agrega el evento click al botón "Editar precio"
+    const editButton = element.querySelector(".edit-product");
+    editButton.addEventListener("click", () => {
+    const priceElement = element.querySelector(".product-price");
+    const inputElement = element.querySelector(".edit-price");
+    const saveButton = element.querySelector(".save-product");
+
+    // Muestra el input para editar el precio y oculta el precio actual
+    inputElement.value = product.price;
+    priceElement.style.display = "none";
+    inputElement.style.display = "inline-block";
+
+    // Muestra el botón "Guardar" y oculta el botón "Editar precio"
+    saveButton.style.display = "inline-block";
+    editButton.style.display = "none";
+  });
+
+  function updateLocalStorage(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+
+
+
+  //GUARDAR DESPUES DE EDITAR PRECIO//
+  
+  // Agrega el evento click al botón "Guardar"
+  const saveButton = element.querySelector(".save-product");
+  saveButton.addEventListener("click", () => {
+  const priceElement = element.querySelector(".product-price");
+  const inputElement = element.querySelector(".edit-price");
+
+  product.price = inputElement.value;
+  product.id = parseInt(element.getAttribute("id")).toString();
+
+  // Actualiza el precio en el elemento HTML correspondiente
+  priceElement.innerHTML = product.price;
+
+  // Oculta el input y muestra el precio actual
+  inputElement.style.display = "none";
+  priceElement.style.display = "inline-block";
+
+  // Oculta el botón "Guardar" y muestra el botón "Editar precio"
+  saveButton.style.display = "none";
+  const editButton = element.querySelector(".edit-product");
+  editButton.style.display = "inline-block";
+
+  // Obtiene los productos del LocalStorage
+  let products = JSON.parse(localStorage.getItem("products"));
+
+  // Busca el producto correspondiente y actualiza su precio
+  products = products.map((p) => {
+    if (p.name === product.name && p.year === product.year) {
+      return product;
+    }
+    return p;
+  });
+
+  // Actualiza el LocalStorage con los nuevos precios
+  updateLocalStorage(products);
+});
+
+
+//BORRAR//
+
+// Agrega el evento click al botón "Eliminar producto"
+const deleteButton = element.querySelector(".delete-product");
+deleteButton.addEventListener("click", (element) => {
+  deleteProduct(element)
+  // element.remove();
+  this.showMessage("Producto eliminado correctamente", "success");
+})
+}
+
+deleteProduct(element) {
+  if (element.name === "delete") {
+    const productCard = element.parentElement.parentElement.parentElement;
+    console.log(productCard)
+    if (!productCard) {
+      return; // salimos de la función si no hay productCard
+    }
+    // const productId = productCard.getAttribute("id");
+    // if (!productId) {
+    //   return; // salimos de la función si no hay productId
+    // }
+    // Eliminar el producto del LocalStorage
+    let products = JSON.parse(localStorage.getItem("products"));
+    products = products.filter((p) => p.id.toString() !== element.id.toString());
+    localStorage.setItem("products", JSON.stringify(products));
+
+    // Eliminar el producto de la lista de productos
+    productCard.remove();
+  }
+}
+
+
+
+/**
    * Reset Form Values
    */
   resetForm() {
     document.getElementById("product-form").reset();
-  }
-
-  deleteProduct(element) {
-    if (element.name === "delete") {
-      element.parentElement.parentElement.remove();
-      this.showMessage("Product Deleted Succsssfully", "success");
-    }
   }
 
   showMessage(message, cssClass) {
@@ -54,3 +185,38 @@ export class UI {
     }, 3000);
   }
 }
+
+const searchInput = document.getElementById("search-input");
+searchInput.addEventListener("input", () => {
+  const searchTerm = searchInput.value.toLowerCase();
+
+  const productCards = document.querySelectorAll(".card.text-center.mb-4");
+  productCards.forEach((card) => {
+    const productName = card.querySelector(`.producto .card-objet`).textContent.toLowerCase();
+
+    if (productName.includes(searchTerm)) {
+      card.style.display = ""; // Mostrar el producto si el término de búsqueda está incluido en el nombre
+    } else {
+      card.style.display = "none"; // Ocultar el producto si no se incluye el término de búsqueda en el nombre
+    }
+  });
+});
+
+const searchForm = document.getElementById("search-form");
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault(); // Evita que se recargue la página al hacer click en el botón
+
+  const searchInput = document.getElementById("search-input");
+  const searchTerm = searchInput.value.toLowerCase(); // Obtener el valor de búsqueda y convertirlo a minúsculas
+
+  const productCards = document.querySelectorAll(".card.text-center.mb-4");
+  productCards.forEach((card) => {
+    const productName = card.querySelector(`.producto .card-objet`).textContent.toLowerCase();
+
+    if (productName.includes(searchTerm)) {
+      card.style.display = ""; // Mostrar el producto si el término de búsqueda está incluido en el nombre
+    } else {
+      card.style.display = "none"; // Ocultar el producto si no se incluye el término de búsqueda en el nombre
+    }
+  });
+});
