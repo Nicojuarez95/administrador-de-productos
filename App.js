41712bea7ce8a94f9ed048aed439135ac391f2af
Nico <juareznico61@gmail.com>
2023-12-14 23:29:12 -0300

    app Nico

--- App.js
+++ App.js
@@ -10,8 +10,7 @@ function callAddProduct(){
 }
 callAddProduct()
 
-document
-  .getElementById("product-form")
+document.getElementById("product-form")
   .addEventListener("submit", function (e) {
     e.preventDefault();
 
